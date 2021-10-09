bf207b3096244771708c4ec2df3c9eb39d018480
student-11 <student-11@student-11.local>
2021-10-09 17:16:57 +0800

    GL02实现嘉宾列表数据的渲染

--- js/ModelService.js
+++ js/ModelService.js
@@ -1,1 +1,15 @@
 //定义model模块
+var model = angular.module('model', []);
+
+model.factory('modelService', function () {
+    return{
+        list:[
+            {name:'小明', phone:'13899999999',gender:'男',state:'邀请中'},
+            {name:'小黄', phone:'13888888888',gender:'男',state:'已接受'},
+            {name:'小红', phone:'13877777777',gender:'女',state:'已拒绝'}
+        ],
+        getList:function(){
+            return this.list
+        }
+    }
+});
