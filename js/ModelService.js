3f9369ecfc0b7cb2024568bff991d01ea2088c7d
student-11 <student-11@student-11.local>
2021-10-09 17:19:12 +0800

    GL04实现嘉宾信息的操作

--- js/ModelService.js
+++ js/ModelService.js
@@ -11,27 +11,24 @@ Guest.ACCEPT='已接受'
 Guest.REFUSE='已拒绝'
 Guest.ALL='全部'
 
+Guest.prototype.accept=function(){
+    this.state=Guest.ACCEPT
+}
+Guest.prototype.refuse=function(){
+    this.state=Guest.REFUSE
+}
+
 model.factory('modelService', function () {
     return{
         list:[
             //{name:'小明', phone:'13899999999',gender:'男',state:'邀请中'},
             //{name:'小黄', phone:'13888888888',gender:'男',state:'已接受'},
             //{name:'小红', phone:'13877777777',gender:'女',state:'已拒绝'}
-        ],
-        listInfo:[
-            {name:'allcount',value:0},
-            {name:'invitecount',value:0},
-            {name:'acceptcount',value:0},
-            {name:'refusecount',value:0},
-        ],
-        getListInfo:function(){
 
-            return this.listInfo;
-        },
+        ],
         getList:function(){
             return this.list
         },
-
         add:function(name, phone, gender){
             var isok=true
             isok=!!(isok&&name&&phone)
@@ -57,6 +54,11 @@ model.factory('modelService', function () {
                 code:0,
                 guest:guest
             }
+        },
+        remove:function(guest){
+            this.list=this.list.filter(function(item){
+                return guest.phone != item.phone;
+            })
         }
     }
 });
